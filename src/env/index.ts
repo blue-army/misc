f4a4533d51db5e917da7d4d2eaff4ae6c7d0d43c
Lucas Natraj <lnatraj@slb.com>
2018-12-29 23:12:15 -0600

    remove env

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,14 @@
 "use strict";
 
 export function env(context: any, req: any) {
-    context.log('one');
     return new Promise<void>(resolve => {
-        context.log('two');
-        context.log('JavaScript HTTP trigger function processed a request.');
-
+        let data = {
+            name: "john wick"
+        }
         context.res = {
-            body: JSON.stringify(process.env, null, "\t")
+            body: JSON.stringify(data, null, "\t")
         };
 
-        context.log('three');
         resolve();
-        context.log('four');
     });
 }
